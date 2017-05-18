23e46b9b642e2bf1c02c10d51f1c07e49959d75b
Alberto Rizzi <rizzaccio@hotmail.it>
2017-05-18 22:13:12 +0200

    Added prod port

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const configureExpress = require('./config/express');
 
 //const db = configureMongoose();
 const app = configureExpress();
-app.listen(3000);
+app.set('port', (process.env.PORT || 3000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 module.exports = app;
-console.log('Server running at http://localhost:3000/');
